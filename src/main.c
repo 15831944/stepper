32f1a6f5afcfa3b0a4992b67591481d419a58695
Zeta <zeta@sagittarii.fr>
2013-06-14 23:49:55 +0200

    add PE7 and PE9 as external interrupts, for limit switchs

--- src/main.c
+++ src/main.c
@@ -87,6 +87,22 @@ void EXTI0_IRQHandler(void)
     EXTI_ClearITPendingBit(EXTI_Line0);
 }
 
+
+void EXTI9_5_IRQHandler(void)
+{
+    TIM4->CCR1 = 0x001;
+    TIM4->CCR2 = TIM4->CCR3 = TIM4->CCR4 = TIM4->CCR1;
+
+    TIM2->ARR -= (TIM2->ARR / 20);
+    freq_request ++;// (freq_request /20);
+
+    //GPIOD->ODR ^= (1 << 13);
+
+    //EXTI->PR |= EXTI_PR_PR0; // reset the interrupt
+    EXTI_ClearITPendingBit(EXTI_Line7);
+    EXTI_ClearITPendingBit(EXTI_Line9);
+}
+
 int main(void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // enable the clock to GPIOD
@@ -138,31 +154,85 @@ int main(void)
     // External interrupt on PA0
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+//    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+//    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
 
-    GPIO_InitTypeDef   GPIO_InitStructure;
-    EXTI_InitTypeDef   EXTI_InitStructure;
-    NVIC_InitTypeDef   NVIC_InitStructure;
+//    GPIO_InitTypeDef   GPIO_InitStructure;
+//    EXTI_InitTypeDef   EXTI_InitStructure;
+//    NVIC_InitTypeDef   NVIC_InitStructure;
 
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+//    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+//    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
+//    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+//    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+//    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+//    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+//    EXTI_Init(&EXTI_InitStructure);
 
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
+//    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+//    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+//    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+//    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+//    NVIC_Init(&NVIC_InitStructure);
 
+
+    // External interrupt on PE9
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource9);
+
+    GPIO_InitTypeDef   GPIO_InitStructureE9;
+    EXTI_InitTypeDef   EXTI_InitStructureE9;
+    NVIC_InitTypeDef   NVIC_InitStructureE9;
+
+    GPIO_InitStructureE9.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructureE9.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructureE9.GPIO_Pin = GPIO_Pin_9;
+    GPIO_Init(GPIOE, &GPIO_InitStructureE9);
+
+    EXTI_InitStructureE9.EXTI_Line = EXTI_Line9;
+    EXTI_InitStructureE9.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructureE9.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructureE9.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructureE9);
+
+    NVIC_InitStructureE9.NVIC_IRQChannel = EXTI9_5_IRQn;
+    NVIC_InitStructureE9.NVIC_IRQChannelPreemptionPriority = 0x0F;
+    NVIC_InitStructureE9.NVIC_IRQChannelSubPriority = 0x0F;
+    NVIC_InitStructureE9.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructureE9);
+
+    // External interrupt on PE9
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource9);
+
+    GPIO_InitTypeDef   GPIO_InitStructureE7;
+    EXTI_InitTypeDef   EXTI_InitStructureE7;
+    NVIC_InitTypeDef   NVIC_InitStructureE7;
+
+    GPIO_InitStructureE7.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructureE7.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStructureE7.GPIO_Pin = GPIO_Pin_7;
+    GPIO_Init(GPIOE, &GPIO_InitStructureE7);
+
+    EXTI_InitStructureE7.EXTI_Line = EXTI_Line7;
+    EXTI_InitStructureE7.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructureE7.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructureE7.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructureE7);
+
+    NVIC_InitStructureE7.NVIC_IRQChannel = EXTI9_5_IRQn;
+    NVIC_InitStructureE7.NVIC_IRQChannelPreemptionPriority = 0x0F;
+    NVIC_InitStructureE7.NVIC_IRQChannelSubPriority = 0x0F;
+    NVIC_InitStructureE7.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructureE7);
 
     /*
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // enable the clock to GPIOA
@@ -182,10 +252,11 @@ int main(void)
 
     while (1)
     {
- /*       if( (GPIOA->IDR & 0x01) == 0x01)
-        {
-            TIM4->CCR1 = 0x001;
-            TIM4->CCR2 = TIM4->CCR3 = TIM4->CCR4 = TIM4->CCR1;
-        }*/
+        //if( (GPIOA->IDR & 0x01) == 0x01)
+        //if( (GPIOE->IDR & (1<<9)) != 0x00)
+//        {
+//            TIM4->CCR1 = 0x001;
+//            TIM4->CCR2 = TIM4->CCR3 = TIM4->CCR4 = TIM4->CCR1;
+//        }
     }
 }
